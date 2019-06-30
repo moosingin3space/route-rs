c5e19058348c128323b4605bd7045d6e43793e17
Collin Valley <collin.valley@gmail.com>
2019-06-29 19:06:00 -0700

    AsyncElement: Prototype for AsyncElement support
    This is an example impl I thought up for Async Element.  In this impl
    the element implements both Future and Stream.  The Future poll is
    the one that is handed to the runtime, and the Stream is what the
    element after this element uses to pop packets from the queue.
    I think this 'hack' might work pretty well since it ends up pretty
    clean, and theoretically we should be able to have two impls of
    poll on the one link element.
    
    There is no test code for this yet. There is one bug in that the
    Future poll returns NotReady when it is unable to do more work,
    and from my reading of Tokio, I do not think it will be woken up
    again, resulting in a hang, unless we implement some sort of
    reactor that will watch the input stream.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,5 @@
-use futures::{Stream, Async, Poll};
-use crossbeam::{Receiver, Sender};
+use futures::{Future, Stream, Async, Poll};
+use std::collections::VecDeque;
 
 pub type ElementStream<Input> = Box<dyn Stream<Item = Input, Error = ()> + Send>;
 
@@ -12,14 +12,14 @@ pub trait Element {
 
 pub struct ElementLink<E: Element> {
     input_stream: ElementStream<E::Input>,
-    core: E
+    element: E
 }
 
 impl<E: Element> ElementLink<E> {
-    pub fn new(input_stream: ElementStream<E::Input>, core: E) -> Self {
+    pub fn new(input_stream: ElementStream<E::Input>, element: E) -> Self {
         ElementLink {
             input_stream,
-            core
+            element
         }
     }
 }
@@ -52,7 +52,7 @@ impl<E: Element> Stream for ElementLink<E> {
         match input_packet_option {
             None => Ok(Async::Ready(None)),
             Some(input_packet) => {
-                let output_packet: E::Output = self.core.process(input_packet);
+                let output_packet: E::Output = self.element.process(input_packet);
                 Ok(Async::Ready(Some(output_packet)))
             },
         }
@@ -63,10 +63,81 @@ pub trait AsyncElement {
     type Input: Sized;
     type Output: Sized;
 
-    fn process(&mut self, packet: Self::Input) -> ElementStream<Self::Output>;
+    fn process(&mut self, packet: Self::Input) -> Self::Output;
 }
 
 pub struct AsyncElementLink<E: AsyncElement> {
-    input_channel: Receiver<E::Input>,
-    output_channel: Sender<E::Output>
+    input_stream: ElementStream<E::Input>,
+    output_queue: VecDeque<E::Output>,
+    queue_capacity: usize,
+    element: E
+}
+
+impl<E: AsyncElement> AsyncElementLink<E> {
+    pub fn new(input_stream: ElementStream<E::Input>, element: E, queue_capacity: usize) -> Self {
+        let output_queue: VecDeque<E::Output> = VecDeque::with_capacity(queue_capacity);
+        AsyncElementLink {
+            input_stream,
+            output_queue,
+            queue_capacity,
+            element
+        }
+    }
+}
+/*
+AsyncElementLink has both Stream and Future because it
+needs one to hand to Tokio, the Future, and another to hand
+to whatever element is after it, the Stream. 
+*/
+impl<E: AsyncElement> Stream for AsyncElementLink<E> {
+    type Item = E::Output;
+    type Error = ();
+
+    /*
+    4 cases: Async::Ready(Some), Async::Ready(None), Async::NotReady, Err
+
+    Async::Ready(Some): We have a packet in the queue that is ready to be returned, pop it and
+    return
+
+    Async::Ready(None): This is never returned
+
+    Async::NotReady: There are no more packets in the queue for us to provide to the output.
+
+    Err: is also handled by the "try_ready!" macro.
+    */
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        let output_packet_option = self.output_queue.pop_front();
+        match output_packet_option {
+            None => { return Ok(Async::NotReady) },
+            Some(output_packet) => {
+                Ok(Async::Ready(Some(output_packet)))
+            },
+        }
+    }
+}
+
+impl<E: AsyncElement> Future for AsyncElementLink<E> {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        println!("Aync Element Poll");
+        loop {
+            /* Check for space in the queue. */
+            if self.output_queue.len() >= self.queue_capacity {
+                return Ok(Async::NotReady)
+            }
+            match try_ready!(self.input_stream.poll()) {
+                Some(input_packet) => {
+                    /* Got a packet, push onto queue*/
+                    let output_packet: E::Output = self.element.process(input_packet);
+                    self.output_queue.push_back(output_packet);
+                },
+                None => {
+                    println!("Consumer received none. End of packet stream");
+                    return Ok(Async::Ready(()))
+                }
+            }
+        }
+    }
 }
